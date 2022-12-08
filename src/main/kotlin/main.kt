55f8f0f1df084cdfabc5bd3f7a852ab4af9c0e98
Joy Test <blossk@outlook.com>
2022-12-08 04:25:22 +0300

    Remarks

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,32 +4,32 @@ fun main() {
     val amount = 3300
     var tax = (amount / 100) * 0.75
     val toTransfer = if (amount > 35) amount - tax else "Минимальная сумма для перевода 35 рублей!"
-    println("Сумма к переводу " + amount + ", Комиссия: " + tax)
-    println("Итого, с учётом комиссии: " + toTransfer)
+    println("Сумма к переводу: ${amount}, Комиссия: ${tax}")
+    println("Итого, с учётом комиссии: ${toTransfer}")
 
     println()
 
     println("TASK #2")
-    val likes = 221
-    val variant = if (likes % 2 == 0) "людям" else "человеку"
-    println("Понравилось " + likes + " " + variant)
+    val likes = 252
+    val count = if ((likes - 1) % 10 == 0) "человеку" else if (likes <= 1) "человеку" else "людям"
+    println("понравилось ${likes} ${count}")
 
     println()
 
     println("# TASK 3")
-    val cart = 17000
-    val vip = true // СТАТУС ПОСТОЯННОГО ПОКУПАТЕЛЯ
-    val discountMax = cart - cart * 0.05
-    val discountVip = cart * 0.01
-    val discountCalc = if (cart < 1000) cart else
-                       if (cart > 1000 && cart < 10000 && vip == true) (cart - discountVip) - 100 else
-                       if (cart > 1000 && cart < 10000) cart - 100 else
-                       if (cart > 10000 && vip == true) discountMax - discountVip else
-                       if (cart > 10000) cart - discountMax else 0
-
+    val cart = 15000
+    val vip = true // Статус покупателя
+    val discountVip   = cart / 100
+    val discountMax = cart / 20
+    var discount =
+        when (cart) {
+            in 0..1000 -> 0
+            in 1000..10000 -> cart - 100
+            in 10000..10000000 -> cart - discountMax
+            else -> 0
+        }
+    val status = if (vip) {discount - discountVip} else discount
     println("КОРЗИНА: ${cart} , СКИДКА ПОСТОЯННОГО ПОКУПАТЕЛЯ: ${if (vip == true) discountVip else "НЕТ"}")
-    println("ИТОГО, СО СКИДКАМИ: ${discountCalc}")
+    println( "Итого, со всеми скидками: ${if (vip == true) ("(-1% VIP) " + status + " рублей") else discount}")
 
 }
-
-
